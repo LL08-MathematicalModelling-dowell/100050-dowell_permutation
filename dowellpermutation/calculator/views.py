f08c1fcdf67b9611f94fc79d24a374d8616f7037
buffetng <ulaitorcodes@gmail.com>
2022-09-02 08:46:00 +0100

    desktop views and templates

--- dowellpermutation/calculator/views.py
+++ dowellpermutation/calculator/views.py
@@ -7,4 +7,20 @@ from django.views import View
 class IndexView(View):
 
     def get(self, request):
-        return render(request, 'index.html')
+        return render(request, 'calculator/index.html')
+
+
+class DesktopTwoView(View):
+
+    def get(self, request):
+        return render(request, 'calculator/desktop-2.html')
+
+class DesktopThreeView(View):
+
+    def get(self, request):
+        return render(request, 'calculator/desktop-3.html')
+
+
+index_view = IndexView.as_view()
+desktop_two_view = DesktopTwoView.as_view()
+desktop_three_view = DesktopThreeView.as_view()
