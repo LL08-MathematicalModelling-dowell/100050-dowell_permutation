bc62c347da51d52e89b27c3742c6e26c5db6ae8f
buffetng <ulaitorcodes@gmail.com>
2022-09-21 07:39:31 +0100

    getting data from each key, to the server

--- dowellpermutation/calculator/urls.py
+++ dowellpermutation/calculator/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
     path('', index_view, name="IndexView"),
     path('desktop/2', desktop_two_view, name="DesktopTwoView"),
     path('desktop/3', desktop_three_view, name="DesktopThreeView"),
-    path('permutations/select', desktop_four_view, name="permutation"),
+    path('permutations/select/', desktop_four_view, name="permutation"),
     path('desktop/5', desktop_five_view, name="DesktopFiveView"),
     path('desktop/6', desktop_six_view, name="DesktopSixView"),
     path('desktop/7', desktop_seven_view, name="DesktopSevenView"),
-    path('permutations/', permutations, name="permutations")
+    path('permutations/', permutations, name="permutations"),
+    path('get-permutation', get_permutation, name="get_permutation")
 ]
