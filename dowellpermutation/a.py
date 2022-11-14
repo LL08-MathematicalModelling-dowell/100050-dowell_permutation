72831dac9c6a56490117f6157cf1c7adc45941ca
bickkysahani <bickky.sahani@gmail.com>
2022-11-14 22:58:20 +0545

    delete

--- dowellpermutation/a.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-import pprint
-from population import targeted_population
-
-
-
-candidate_response = targeted_population('hr_hiring','dowelltraining',  ['full_name'], 'life_time')
-print(candidate_response)
